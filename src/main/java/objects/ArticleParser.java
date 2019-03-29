c895a1f1d14a089d277ffc8e4b2be3df55027031
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-29 11:49:09 +0100

    Finish article parser

--- src/main/java/objects/ArticleParser.java
+++ src/main/java/objects/ArticleParser.java
@@ -2,9 +2,37 @@ package objects;
 
 import utils.Parser;
 import java.io.File;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
 
 public class ArticleParser implements Parser {
-    public ClassificationObject[] parseFile(File file) {
-        return new ClassificationObject[1];
+    public ClassificationObject[] parseFile(File file) throws IOException {
+        Document document = Jsoup.parse(file, "utf-8");
+        Elements elements = document.getElementsByTag("REUTERS");
+
+        ClassificationObject[] classificationObjects = new ClassificationObject[elements.size()];
+
+        for (int e = 0; e < classificationObjects.length; e++) {
+            Element element = elements.get(e);
+
+            Elements title = element.getElementsByTag("TITLE");
+            Elements places = element.getElementsByTag("PLACES");
+            Elements body = element.getElementsByTag("BODY");
+
+            Elements places_items = places.select("D");
+            String[] places_names = new String[places_items.size()];
+            for (int i = 0; i < places_names.length; i++) {
+                places_names[i] = places_items.get(i).text();
+            }
+
+            Article article = new Article(title.text(), places_names, body.text());
+            classificationObjects[e] = article;
+        }
+
+        return classificationObjects;
     }
 }
--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -2,7 +2,8 @@ package utils;
 
 import objects.ClassificationObject;
 import java.io.File;
+import java.io.IOException;
 
 public interface Parser {
-    ClassificationObject[] parseFile(File file);
+    ClassificationObject[] parseFile(File file) throws IOException;
 }
