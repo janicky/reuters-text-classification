9a1b4818277b54d53d84516638f85969e69f0731
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-29 11:31:16 +0100

    Add article parser class

---
+++ src/main/java/objects/ArticleParser.java
@@ -0,0 +1,10 @@
+package objects;
+
+import utils.Parser;
+import java.io.File;
+
+public class ArticleParser implements Parser {
+    public ClassificationObject[] parseFile(File file) {
+        return new ClassificationObject[1];
+    }
+}
