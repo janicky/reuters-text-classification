46f0fc1191c08fb8f29ed6e91fd8a96d97cb90da
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-29 19:09:56 +0100

    Add wordsOccurrences method to operations class

--- src/main/java/utils/Operations.java
+++ src/main/java/utils/Operations.java
@@ -1,13 +1,30 @@
 package utils;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Operations {
-    public static String normalizeText(String text) {
+    public static String normalizeText(final String text) {
         return text.trim().replaceAll("[^A-Za-z\\s+]", "").toLowerCase();
     }
 
-    public static String[] splitText(String text) {
+    public static String[] splitText(final String text) {
         return text.split("\\s+");
+    }
+
+    public static Map<String, Integer> wordsOccurrences(String[] dictionary, String[] text) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : text) {
+            for (String dict : dictionary) {
+                if (word.equals(dict)) {
+                    if (map.containsKey(word)) {
+                        map.put(word, map.get(word) + 1);
+                    } else {
+                        map.put(word, 1);
+                    }
+                }
+            }
+        }
+        return map;
     }
 }
