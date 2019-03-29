5db3b294294c050844445590c68dfc907a1e4201
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-29 18:21:06 +0100

    Add cleanText method to operations

--- src/main/java/utils/Operations.java
+++ src/main/java/utils/Operations.java
@@ -1,6 +1,10 @@
 package utils;
 
 public class Operations {
+    public static String cleanText(String text) {
+        return text.trim().replaceAll("[^A-Za-z\\s+]", "");
+    }
+
     public static String[] vectorizeText(String text) {
         return text.split("\\s+");
     }
--- src/test/java/utils/OperationsTest.java
+++ src/test/java/utils/OperationsTest.java
@@ -9,6 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class OperationsTest {
 
     @Test
+    void cleanText() {
+        String text = "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.";
+        String cleanedText = Operations.cleanText(text);
+        assertEquals("Two things are infinite the universe and human stupidity and Im not sure about the universe", cleanedText);
+    }
+
+    @Test
     void vectorizeText() {
         String text = "With increasing distance, our knowledge fades, and fades rapidly.";
         String[] vectorizedText = Operations.vectorizeText(text);
