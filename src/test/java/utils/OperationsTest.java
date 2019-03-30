0aaf567d1d776bb8d00244547e7bcb4f34c5f6cc
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-30 18:55:28 +0100

    Add words occurrence tests

--- src/test/java/utils/OperationsTest.java
+++ src/test/java/utils/OperationsTest.java
@@ -3,6 +3,7 @@ package utils;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,5 +25,16 @@ class OperationsTest {
         for (int i = 0; i < vectorizedText.length; i++) {
             assertEquals(expected[i], vectorizedText[i]);
         }
+    }
+
+    @Test
+    void wordsOccurrences() {
+        String[] dictionary = new String[] { "neil", "armstrong", "astronaut", "engineer", "first", "moon" };
+        String text = "Neil Alden Armstrong was an American astronaut and aeronautical engineer who was the first person to walk on the Moon. Armstrong prepared his famous epigram on his own.";
+        String[] prepared = Operations.splitText(Operations.normalizeText(text));
+
+        Map<String, Integer> map = Operations.wordsOccurrences(dictionary, prepared);
+        assertEquals(1, map.get("astronaut").intValue());
+        assertEquals(2, map.get("armstrong").intValue());
     }
 }
