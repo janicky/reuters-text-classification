b27363d7617294628a14f4966291cf217f633eb1
Patryk Janicki <kontakt.janicki@gmail.com>
2019-03-30 19:41:11 +0100

    Add density extraction method

--- src/main/java/utils/Extractor.java
+++ src/main/java/utils/Extractor.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.util.Arrays;
+import java.util.Map;
+
 public class Extractor {
     public static boolean[] binaryExtraction(String[] dictionary, String[] text) {
         boolean[] output = new boolean[dictionary.length];
@@ -15,6 +18,20 @@ public class Extractor {
             output[i] = found;
         }
 
+        return output;
+    }
+
+    public static double[] densityExtraction(String[] dictionary, String[] text) {
+        double[] output = new double[dictionary.length];
+        Map<String, Integer> occurrences = Operations.wordsOccurrences(dictionary, text);
+
+        if (text.length > 0) {
+            for (int i = 0; i < dictionary.length; i++) {
+                output[i] = occurrences.get(dictionary[i]) / (double) text.length;
+            }
+        } else {
+            Arrays.fill(output, 0);
+        }
         return output;
     }
 }
