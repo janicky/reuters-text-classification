1a9fd62e7f57ab9f66ff4444baeb5d3cbf0d5250
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-05 19:08:14 +0200

    Add selectLabel method tests

--- src/test/java/utils/OperationsTest.java
+++ src/test/java/utils/OperationsTest.java
@@ -16,13 +16,19 @@ class OperationsTest {
     private IClassificationObject object_2;
     private IClassificationObject object_3;
     private IClassificationObject object_4;
+    private Map<IClassificationObject, Double> objectsMap = new HashMap<>();
 
     @BeforeEach
     void initialize() {
         object_1 = new Article("TT1", new String[] { "P1", "P2" }, "TX1" );
         object_2 = new Article("TT2", new String[] { "P3" }, "TX2" );
         object_3 = new Article("TT3", new String[] { "P4" }, "TX3" );
-        object_4 = new Article("TT3", new String[] { "P4" }, "TX3" );
+        object_4 = new Article("TT3", new String[] { "P1" }, "TX3" );
+
+        objectsMap.put(object_1, 0.5);
+        objectsMap.put(object_2, 15.2);
+        objectsMap.put(object_3, 1.5);
+        objectsMap.put(object_4, 0.4);
     }
 
     @Test
@@ -56,19 +62,15 @@ class OperationsTest {
 
     @Test
     void selectObjects() {
-        Map<IClassificationObject, Double> map = new HashMap<>();
-        map.put(object_1, 0.5);
-        map.put(object_2, 15.2);
-        map.put(object_3, 1.5);
-        map.put(object_4, 0.4);
-
-        IClassificationObject[] selectedObjects = Operations.selectObjects(map, 2);
+        IClassificationObject[] selectedObjects = Operations.selectObjects(objectsMap, 2);
         assertEquals(object_4, selectedObjects[0]);
         assertEquals(object_1, selectedObjects[1]);
     }
 
     @Test
     void selectLabel() {
-
+        IClassificationObject[] selectedObjects = Operations.selectObjects(objectsMap, 2);
+        String label = Operations.selectLabel(selectedObjects);
+        assertEquals("P1", label);
     }
 }
