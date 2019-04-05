e72475d0d5b59d22243fc71414c11fac4950c372
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-05 19:01:03 +0200

    Move articles to beforeEach method in classification tests file

--- src/test/java/utils/OperationsTest.java
+++ src/test/java/utils/OperationsTest.java
@@ -2,6 +2,8 @@ package utils;
 
 import data_models.Article;
 import interfaces.IClassificationObject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -9,6 +11,19 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OperationsTest {
+
+    private IClassificationObject object_1;
+    private IClassificationObject object_2;
+    private IClassificationObject object_3;
+    private IClassificationObject object_4;
+
+    @BeforeEach
+    void initialize() {
+        object_1 = new Article("TT1", new String[] { "P1", "P2" }, "TX1" );
+        object_2 = new Article("TT2", new String[] { "P3" }, "TX2" );
+        object_3 = new Article("TT3", new String[] { "P4" }, "TX3" );
+        object_4 = new Article("TT3", new String[] { "P4" }, "TX3" );
+    }
 
     @Test
     void normalizeText() {
@@ -42,15 +57,18 @@ class OperationsTest {
     @Test
     void selectObjects() {
         Map<IClassificationObject, Double> map = new HashMap<>();
-        IClassificationObject o1 = new Article("TT1", new String[] { "P1", "P2" }, "TX1" );
-        map.put(o1, 0.5);
-        map.put(new Article("TT2", new String[] { "P3" }, "TX2" ), 15.2);
-        map.put(new Article("TT3", new String[] { "P4" }, "TX3" ), 1.5);
-        IClassificationObject o2 = new Article("TT3", new String[] { "P4" }, "TX3" );
-        map.put(o2, 0.4);
+        map.put(object_1, 0.5);
+        map.put(object_2, 15.2);
+        map.put(object_3, 1.5);
+        map.put(object_4, 0.4);
 
         IClassificationObject[] selectedObjects = Operations.selectObjects(map, 2);
-        assertEquals(o2, selectedObjects[0]);
-        assertEquals(o1, selectedObjects[1]);
+        assertEquals(object_4, selectedObjects[0]);
+        assertEquals(object_1, selectedObjects[1]);
+    }
+
+    @Test
+    void selectLabel() {
+
     }
 }
