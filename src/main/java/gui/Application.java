29ece0d4bc0b6dbb642dcda5b1f822b65514637f
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-07 12:33:05 +0200

    Initialize graphic user interface

---
+++ src/main/java/gui/Application.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.*;
+
+public class Application {
+    public JPanel mainPanel;
+    private JFrame frame;
+
+    public Application(JFrame frame) {
+        this.frame = frame;
+    }
+}
