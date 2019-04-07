9effd7e4d4e099c91df5abadeac4bd37d40be0d2
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-07 13:57:10 +0200

    Add swing controls

--- src/main/java/gui/Application.java
+++ src/main/java/gui/Application.java
@@ -1,12 +1,39 @@
 package gui;
 
 import javax.swing.*;
+import java.lang.reflect.Method;
 
 public class Application {
     public JPanel mainPanel;
+    private JList extractorsList;
+    private JRadioButton euclideanRadioButton;
+    private JRadioButton chebyshevRadioButton;
+    private JRadioButton radioButton1;
+    private JSlider parameterSlider;
+    private JLabel kvalue;
     private JFrame frame;
 
     public Application(JFrame frame) {
         this.frame = frame;
+        try {
+            fillExtractors();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        parameterSlider.addChangeListener(e -> {
+            System.out.println(parameterSlider.getValue());
+        });
+    }
+
+    private void fillExtractors() throws ClassNotFoundException {
+        Class c = Class.forName("classification.utils.Extractor");
+        DefaultListModel model = new DefaultListModel();
+        extractorsList.setModel(model);
+        Method[] methods = c.getDeclaredMethods();
+
+        for (int i = 0; i < methods.length; i++) {
+            model.add(i, methods[i].getName());
+        }
     }
 }
