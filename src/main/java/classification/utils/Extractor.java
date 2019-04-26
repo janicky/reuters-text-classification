89b94935a3250095c4b234cf530ef1fc2244f9c7
KJachimstal <k,jachimstal@gmail.com>
2019-04-26 20:22:50 +0200

    Change return type change from double to Map

--- src/main/java/classification/utils/Extractor.java
+++ src/main/java/classification/utils/Extractor.java
@@ -1,65 +1,82 @@
 package classification.utils;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Extractor {
-    public static double occurrencesCountExtraction(String[] dictionary, String[] text) {
-        int occurrences = 0;
+    public static Map occurrencesCountExtraction(String[] dictionary, String[] text) {
+        Map<String, Double> occurrencesCount = new LinkedHashMap<>();
 
         for (String dict : dictionary) {
-            for (String word : text) {
-                if (word.equals(dict)) {
-                    occurrences++;
-                    break;
-                }
-            }
-        }
-        return (double) occurrences;
-    }
-
-    public static double occurrencesSumExtraction(String[] dictionary, String[] text) {
-        int occurrences = 0;
-
-        for (String dict : dictionary) {
+            int occurrences = 0;
             for (String word : text) {
                 if (word.equals(dict)) {
                     occurrences++;
                 }
             }
+            occurrencesCount.put(dict, (double)occurrences);
         }
-        return (double) occurrences;
+        return occurrencesCount;
     }
 
-    public static double densityExtraction(String[] dictionary, String[] text) {
-        double occurrences = occurrencesSumExtraction(dictionary, text);
+    public static Map occurrencesSumExtraction(String[] dictionary, String[] text) {
+        Map<String, Double> occurrencesSum = new LinkedHashMap<>();
+        double occurrences = 0;
+
+        for (String dict : dictionary) {
+            for (String word : text) {
+                if (word.equals(dict)) {
+                    occurrences++;
+                }
+            }
+        }
+        occurrencesSum.put("occurrencesSum", occurrences);
+        return occurrencesSum;
+    }
+
+    public static Map densityExtraction(String[] dictionary, String[] text) {
+        Map<String, Double> result = new LinkedHashMap<>();
+        Map<String, Double> occSum = occurrencesSumExtraction(dictionary, text);
         if (text.length == 0) {
-            return 0;
+            result.put("density", 0d);
+            return result;
         }
-        return occurrences / (double) text.length;
+        result.put("density", (occSum.get("occurrencesSum") / text.length));
+        return result;
     }
 
-    public static double averageDistanceExtraction(String[] dictionary, String[] text) {
-        int sum = 0;
-        int occurrences = 0;
+    public static Map averageDistanceExtraction(String[] dictionary, String[] text) {
+        Map<String, Double> averageDistance = new LinkedHashMap<>();
+        double sum = 0;
+        double occurrences = 0;
         for (String dict : dictionary) {
             for (int i = 0; i < text.length; i++) {
                 if (text[i].equals(dict)) {
                     sum += i;
                     occurrences++;
+
                 }
             }
         }
+        averageDistance.put("distance", sum/occurrences);
 
         if (occurrences == 0) {
-            return 0;
+            averageDistance.put("distance", 0d);
+            return averageDistance;
         }
-        return sum / (double) occurrences;
+        return averageDistance;
     }
 
-    public static double wordsCountExtraction(String[] dictionary, String[] text) {
-        return (double) text.length;
+    public static Map wordsCountExtraction(String[] dictionary, String[] text) {
+        Map<String, Double> wordsCount = new LinkedHashMap<>();
+        wordsCount.put("wordsCount", (double)text.length);
+        return wordsCount;
     }
 
-    public static double wordsDistractionExtraction(String[] dictionary, String[] text) {
-        int start = -1, sum = 0, words = 0;
+    public static Map wordsDistractionExtraction(String[] dictionary, String[] text) {
+        Map<String, Double> wordsDistraction = new LinkedHashMap<>();
+        double start = -1, sum = 0, words = 0;
 
         for (int i = 0; i < text.length; i++) {
             for (String dict : dictionary) {
@@ -73,7 +90,7 @@ public class Extractor {
                 }
             }
         }
-
-        return ((words - 1) > 0 ? (sum / (double) text.length) / (double)(words - 1) : 0);
+        wordsDistraction.put("wordsDistraction", (words - 1) > 0 ? (sum / text.length) / (double)(words - 1) : 0);
+        return wordsDistraction;
     }
 }
