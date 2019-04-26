a09c51aa9e567b0969cbc24e65bae88c28809ab9
KJachimstal <k,jachimstal@gmail.com>
2019-04-26 20:56:30 +0200

    add manhattan metric

---
+++ src/main/java/classification/metrics/Manhattan.java
@@ -0,0 +1,15 @@
+package classification.metrics;
+
+import classification.abstracts.Metric;
+import classification.utils.Feature;
+
+public class Manhattan extends Metric {
+    public double calculate() {
+        double sum = 0;
+        for (Feature feature : features) {
+            sum += Math.abs(feature.getX() - feature.getY());
+        }
+
+        return sum;
+    }
+}
