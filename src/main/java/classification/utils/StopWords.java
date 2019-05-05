758dd083e7bcf207d588a52dda2ca276d3dfd233
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-05 18:27:07 +0200

    Add stop words

---
+++ src/main/java/classification/utils/StopWords.java
@@ -0,0 +1,35 @@
+package classification.utils;
+
+import classification.data_models.IClassificationObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StopWords {
+    private String[] stopWords;
+    private IClassificationObject[] objects;
+
+    public StopWords(IClassificationObject[] objects) {
+        this.objects = objects;
+    }
+
+    public void generate() {
+
+    }
+
+    public void loadFromFile(String filename) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(filename));
+        List<String> words = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            words.add(scanner.nextLine());
+        }
+        scanner.close();
+    }
+
+    public String[] getStopWords() {
+        return stopWords;
+    }
+}
