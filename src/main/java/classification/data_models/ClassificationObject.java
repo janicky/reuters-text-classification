bf7ace3eb1274f2b7dd7336161c7d4917b6f6144
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-05 17:26:01 +0200

    Change getVectorizedText method in ClassificationObject class

--- src/main/java/classification/data_models/ClassificationObject.java
+++ src/main/java/classification/data_models/ClassificationObject.java
@@ -1,11 +1,9 @@
 package classification.data_models;
 
-import classification.data_models.IClassificationObject;
 import classification.utils.Operations;
 
 public abstract class ClassificationObject implements IClassificationObject {
     public String[] getVectorizedText() {
-        String normalized = Operations.normalizeText(getText());
-        return Operations.splitText(normalized);
+        return Operations.splitText(getText());
     }
 }
