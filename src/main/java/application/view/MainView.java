63c2eaecf0cc6a9a7b8acb4347b942d392c6ef84
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-08 12:12:47 +0200

    Add tabbed pane to main view

--- src/main/java/application/view/MainView.java
+++ src/main/java/application/view/MainView.java
@@ -4,8 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainView extends JFrame {
+    private JTabbedPane mainPane;
+    private JPanel mainPanel;
+
     public MainView(String title) throws HeadlessException {
         setTitle(title);
+        setContentPane(mainPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
     }
