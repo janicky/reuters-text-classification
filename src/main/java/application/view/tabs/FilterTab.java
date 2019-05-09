ae254254f3e0ccfa0f6fb5dfa9a8d5b33d14c523
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-09 13:00:18 +0200

    Add lists to filter tab

--- src/main/java/application/view/tabs/FilterTab.java
+++ src/main/java/application/view/tabs/FilterTab.java
@@ -1,11 +1,30 @@
 package application.view.tabs;
 
+import classification.data_models.IClassificationObject;
+
 import javax.swing.*;
 
 public class FilterTab {
     private JPanel mainPanel;
+    private JList labelsList;
+    private JList objectsList;
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+    public void setLabels(String[] labels) {
+        DefaultListModel listModel = new DefaultListModel();
+        for (String label : labels) {
+            listModel.addElement(label);
+        }
+        labelsList.setModel(listModel);
+    }
+    public void setObjects(IClassificationObject[] objects) {
+        DefaultListModel listModel = new DefaultListModel();
+        for (IClassificationObject object : objects) {
+            listModel.addElement(object);
+        }
+        objectsList.setModel(listModel);
     }
 }
