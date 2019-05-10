fdfcd60717f8ad51a97fda5867e75b874813fafc
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-10 10:28:50 +0200

    Add keywords tab listeners

--- src/main/java/application/view/tabs/KeywordsTab.java
+++ src/main/java/application/view/tabs/KeywordsTab.java
@@ -1,6 +1,8 @@
 package application.view.tabs;
 
 import javax.swing.*;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ActionListener;
 
 public class KeywordsTab {
     private JPanel mainPanel;
@@ -11,8 +13,34 @@ public class KeywordsTab {
     private JButton removeSelectedKeywordButton;
     private JSpinner keywordsSignificanceSpinner;
     private JButton generateKeywordsButton;
+    private DefaultListModel listModel;
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+    public void addGenerateKeywordsButtonListener(ActionListener listener) {
+        generateKeywordsButton.addActionListener(listener);
+    }
+
+    public void addKeywordButtonListener(ActionListener listener) {
+        addKeywordButton.addActionListener(listener);
+    }
+
+    public void addRemoveSelectedKeywordButtonListener(ActionListener listener) {
+        removeSelectedKeywordButton.addActionListener(listener);
+    }
+
+    public void addKeywordsSignificanceSpinnerListener(ChangeListener listener) {
+        keywordsSignificanceSpinner.addChangeListener(listener);
+    }
+
+    public void setKeywords(String[] keywords) {
+        listModel = new DefaultListModel();
+        for (String keyword : keywords) {
+            listModel.addElement(keyword);
+        }
+        keywordsList.setModel(listModel);
+        objectsCount.setText(Integer.toString(keywords.length));
     }
 }
