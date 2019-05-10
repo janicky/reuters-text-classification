a79dfb027e6ffdf3b8d8620367b95d442e0f9ef0
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-10 17:25:09 +0200

    Design classification tab

--- src/main/java/application/view/tabs/ClassificationTab.java
+++ src/main/java/application/view/tabs/ClassificationTab.java
@@ -4,6 +4,13 @@ import javax.swing.*;
 
 public class ClassificationTab {
     private JPanel mainPanel;
+    private JCheckBox learningSetCheckBox;
+    private JComboBox comboBox1;
+    private JPanel metricComboBox;
+    private JComboBox similarityComboBox;
+    private JSlider kSlider;
+    private JLabel kLabel;
+    private JButton startClassificationButton;
 
     public JPanel getMainPanel() {
         return mainPanel;
