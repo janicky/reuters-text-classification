8b16adc9036b37a726391e9944262db71fa319e7
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-10 10:16:39 +0200

    Add keywords tab form design

--- src/main/java/application/view/tabs/KeywordsTab.java
+++ src/main/java/application/view/tabs/KeywordsTab.java
@@ -4,6 +4,13 @@ import javax.swing.*;
 
 public class KeywordsTab {
     private JPanel mainPanel;
+    private JLabel objectsCount;
+    private JList keywordsList;
+    private JFormattedTextField keywordInput;
+    private JButton addKeywordButton;
+    private JButton removeSelectedKeywordButton;
+    private JSpinner significanceSpinner;
+    private JButton generateButton;
 
     public JPanel getMainPanel() {
         return mainPanel;
