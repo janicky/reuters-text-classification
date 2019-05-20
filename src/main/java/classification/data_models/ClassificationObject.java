8ac798fbaff0dbfe6012e3e154db7bf3e8aec05b
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-20 21:09:17 +0200

    Add equals and hash to classification object class

--- src/main/java/classification/data_models/ClassificationObject.java
+++ src/main/java/classification/data_models/ClassificationObject.java
@@ -4,14 +4,19 @@ import classification.features.IFeature;
 import classification.utils.Operations;
 
 import java.util.Map;
+import java.util.Objects;
 
 public abstract class ClassificationObject implements IClassificationObject {
     private String[] vectorizedText;
     private Map<String, IFeature> featuresVector;
 
+    private static int index = 0;
+    private int id;
+
     public ClassificationObject(String text) {
         String cleaned = Operations.removeSpecialCharacters(text);
         vectorizedText = Operations.splitText(cleaned);
+        id = index++;
     }
 
     public String[] getVectorizedText() {
@@ -28,5 +33,18 @@ public abstract class ClassificationObject implements IClassificationObject {
 
     public void setFeaturesVector(Map<String, IFeature> featuresVector) {
         this.featuresVector = featuresVector;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClassificationObject that = (ClassificationObject) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
