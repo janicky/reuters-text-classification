b7582c24c617e2c9ce4ad4d168fb2b12f7d4fbfc
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-20 21:06:05 +0200

    Add new keywords generator method

--- src/main/java/classification/utils/Keywords.java
+++ src/main/java/classification/utils/Keywords.java
@@ -1,28 +1,27 @@
 package classification.utils;
 
+import classification.data_models.ClassificationObject;
 import classification.data_models.IClassificationObject;
 import classification.features.TermFrequency;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Keywords {
 
     IClassificationObject[] objects;
+    String[][] documents;
     List<String> keywords = new ArrayList<>();
 
     public Keywords(IClassificationObject[] objects) {
         this.objects = objects;
-    }
-
-    public void generate(double significance) {
-        String[][] documents = new String[objects.length][];
+        documents = new String[objects.length][];
         for (int i = 0; i < objects.length; i++) {
             documents[i] = objects[i].getVectorizedText();
         }
+    }
 
+    public void generate(double significance) {
         List<String> newKeywords = new ArrayList<>();
 
         int finished = 0;
@@ -38,6 +37,42 @@ public class Keywords {
             }
             finished++;
             System.out.println(finished + " / " + objects.length);
+        }
+        keywords = newKeywords;
+    }
+
+    public void generate(int count) {
+        LinkedHashMap<String, Map<String, Double>> labels = new LinkedHashMap<>();
+
+        int finished = 0;
+        for (IClassificationObject object : objects) {
+            String[] words = object.getVectorizedText();
+            String label = object.getLabels()[0];
+            if (!labels.containsKey(label)) {
+                labels.put(label, new HashMap<>());
+            }
+            for (String word : words) {
+                Map map = labels.get(label);
+                if (!map.containsKey(word) && word.length() > 2) {
+                    double tfidf = TermFrequency.termFrequencyInverseFrequency(documents, words, word);
+                    map.put(word, tfidf);
+                }
+            }
+            finished++;
+            System.out.println(finished + " / " + objects.length);
+        }
+
+        List<String> newKeywords = new ArrayList<>();
+        for (Map.Entry<String, Map<String, Double>> entry : labels.entrySet()) {
+            Map sorted = Operations.sortByValuesDesc(entry.getValue());
+            int i = 0;
+            for (Object object : sorted.entrySet()) {
+                if (i < count) {
+                    Map.Entry<String, Double> keyword = (Map.Entry) object;
+                    newKeywords.add(keyword.getKey());
+                    i++;
+                }
+            }
         }
         keywords = newKeywords;
     }
